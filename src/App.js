fb000521e6baa4c79a94854bdafa4d27eb32ff9e
Erik Konner <erikkonner@gmail.com>
2019-04-14 13:27:04 -0600

    Filter method for questions

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import GameContainer from './Components/GameContainer.js';
 import './scss/App.scss';
 import dataSet from './dataSet.js'
+import finnLoad from './images/finn-load.jpg';
+import lspLoad from './images/lsp-load.jpg';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       isLoading: true,
-      filteredQuestions:[],
       questions: dataSet.cards
     }
-
+    console.log(this.state.questions)
   }
 
   render() {
+    let gameContainer = this.state.questions.length ? <GameContainer questions={this.state.questions} /> 
+    : <img src={finnLoad}>loading...</img>;
     return (
       <div className="App">
        <main>
@@ -23,15 +26,8 @@ class App extends Component {
                 <h2> Master String Prototypes </h2>
                 <h3>Click on a difficulty to begin!</h3>
             </header>
-            <section>
-                <button onClick>Medium</button>
-                <button>Easy</button>
-                <button>Hard</button>
-            </section>
-        <GameContainer gameContainer= {GameContainer} /> 
-        <section>
-
-        </section>
+            {gameContainer}
+            
         </main>
       </div>
     );
--- src/Components/GameContainer.js
+++ src/Components/GameContainer.js
@@ -6,12 +6,47 @@ class GameContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            questionList: [],
+            selectedQuestions:[]
         }
+        this.filterQuestions = this.filterQuestions.bind(this);
+        
     }
+    componentDidMount() {
+        this.getQuestions();
+    }
+
+    getQuestions() {
+        const questionList = this.props.questions;
+        console.log(questionList)
+        this.setState({
+            questionList: questionList
+        })
+    }
+
+    saveToStorage() {
+        localStorage.setItem('questionsRemaining', JSON.stringify(this.state.questionList));
+    }
+
+    filterQuestions(e) {
+        let value = e.target.innerText.toLowerCase();
+        let filteredQuestions = this.state.questionList.filter(question => {
+            return question.category === value;
+        });
+        this.setState({
+            selectedQuestions: filteredQuestions
+        })
+        console.log(this.state.selectedQuestions);
+    }
+
     render() {
+        let card =  <Card selectedQuestions={this.state.selectedQuestions} /> : null;
         return ( 
-       <div></div>
+       <section>
+           <button onClick={this.filterQuestions}>Hard</button>
+           <button onClick={this.filterQuestions}>Easy</button>
+           <button onClick={this.filterQuestions}>Medium</button>
+       </section>
         )   
     }
 
