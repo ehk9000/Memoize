b1f70dfcf3bece1fc3fd32942964309474d89d10
Erik Konner <erikkonner@gmail.com>
2019-04-13 19:32:28 -0600

    Basic UI and Component Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GameContainer from './Components/GameContainer.js';
+import './scss/App.scss';
+import dataSet from './dataSet.js'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      filteredQuestions:[],
+      questions: dataSet.cards
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <main>
+            <header>
+                <h1>Adventure Time!</h1>      
+                <h2> Master String Prototypes </h2>
+                <h3>Click on a difficulty to begin!</h3>
+            </header>
+            <section>
+                <button onClick={}>Medium</button>
+                <button>Easy</button>
+                <button>Hard</button>
+            </section>
+        <GameContainer gameContainer= {GameContainer} /> 
+        <section>
+
+        </section>
+        </main>
       </div>
     );
   }
---
+++ src/Components/GameContainer.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import ScoreBoard from './ScoreBoard';
+import '../scss/GameContainer.scss';
+
+class GameContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return ( 
+       <div></div>
+        )   
+    }
+
+}
+
+export default GameContainer;
